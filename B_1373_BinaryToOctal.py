21e69276191a811372da94a6941c5375b8f98159
jacob3015 <jacob3015@gmail.com>
2021-07-31 15:07:43 +0900

    Modify comments

--- B_1373_BinaryToOctal.py
+++ B_1373_BinaryToOctal.py
@@ -12,7 +12,7 @@ Output:
 def solution():
     """주어진 2진수를 10진수로 변환 후, 10진수를 8진수로 변환하여 출력한다.
     Extra explanation:
-        Python에서 2진수는 접두어로 '0b'를 붙여 표현하므로 주어진 2진수를 String으로 입력 받아 '0b'를 접두어로 붙여준다.
+        Python에서 2진수는 접두어로 '0b'를 붙혀 표현하므로 주어진 2진수를 String으로 입력 받아 '0b'를 접두어로 붙혀준다.
         int() method에 base를 2진수로 넘겨주며 2진수를 10진수로 변환한다.
         format() method에 formatting type을 'o'(Octal) 8진수로 넘겨주며 10진수를 접두어를 제외한 8진수 String으로 변환한다.
     
