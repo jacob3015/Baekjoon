5eb4074565e6cfc13ea758cf12e3396797c6c4f8
jacob3015 <jacob3015@gmail.com>
2021-08-19 15:07:24 +0900

    Add visualization

--- B_1912_ConsecutiveSum.py
+++ B_1912_ConsecutiveSum.py
@@ -14,6 +14,8 @@ Output:
 
 def solution():
     """주어진 수열의 연속된 부분 수열의 합 중 최댓값을 구해 출력한다.
+    Extra explanation:
+        DP의 memoization을 활용해 문제를 해결하며 풀이과정은 solution_visualization()을 통해 시각화해 볼 수 있다.
 
     Variables:
         n : 주어진 수열의 수 개수
@@ -29,7 +31,44 @@ def solution():
 
     for i in range(1, n):
         sequence[i] = max(sequence[i], sequence[i-1] + sequence[i])
-        
+
     print(max(sequence))
+
+def solution_visualization(n, sequence):
+    """DP의 memoization을 활용한 문제 해결의 풀이 과정을 시각화 한다.
+
+    Example:
+        >>> 10                                              : input n
+        >>> 10 -4 3 1 5 6 -35 12 21 -1                      : input sequence
+        >>> n : 10                                          : outputs
+        >>> start : [10, -4, 3, 1, 5, 6, -35, 12, 21, -1]
+        >>> ----------------------------------------
+        >>> [10, 6, 3, 1, 5, 6, -35, 12, 21, -1]
+        >>> [10, 6, 9, 1, 5, 6, -35, 12, 21, -1]
+        >>> [10, 6, 9, 10, 5, 6, -35, 12, 21, -1]
+        >>> [10, 6, 9, 10, 15, 6, -35, 12, 21, -1]
+        >>> [10, 6, 9, 10, 15, 21, -35, 12, 21, -1]
+        >>> [10, 6, 9, 10, 15, 21, -14, 12, 21, -1]
+        >>> [10, 6, 9, 10, 15, 21, -14, 12, 21, -1]
+        >>> [10, 6, 9, 10, 15, 21, -14, 12, 33, -1]
+        >>> [10, 6, 9, 10, 15, 21, -14, 12, 33, 32]
+        >>> ----------------------------------------
+        >>> end : [10, 6, 9, 10, 15, 21, -14, 12, 33, 32]
+        >>> result : 33
+    """
+    print(f'n : {n}')
+    print('start :', end=' ')
+    print(sequence)
+
+    print('------------------------------------')
+    for i in range(1, n):
+        sequence[i] = max(sequence[i], sequence[i-1] + sequence[i])
+        print(sequence)
+    print('------------------------------------')
+
+    
+    print('end :', end=' ')
+    print(sequence)
+    print(f'result : {max(sequence)}')
 
 solution()
